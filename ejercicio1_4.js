f877d5ec0b4eb2d94b1c68ec97d7e8913041eece
nijinsky14 <francogaibazzi14@gmail.com>
2022-09-22 09:34:29 -0300

    agrego ejercicio 4

---
+++ ejercicio1_4.js
@@ -0,0 +1,22 @@
+var entero = 6;
+var decimal = 6.66;
+var hexadecimal = 0xf00d;
+var binario = 666;
+var octal = 484;
+var ceroNegativo = -0;
+var enRealiadadNumero = NaN;
+var mayorNumero = Number.MAX_VALUE;
+var elNumeroMasGrande = Infinity;
+var miembros = [
+    entero,
+    decimal,
+    hexadecimal,
+    binario,
+    octal,
+    ceroNegativo,
+    enRealiadadNumero,
+    mayorNumero,
+    elNumeroMasGrande
+];
+miembros[0] = 12345;
+console.log('[Ejercicio 1.4]', miembros);
